e9b6378a13a716c65ae2b5d27c9e3c5264c71d0c
Gilbert Sampaio <52259778+Gilbertsampaio@users.noreply.github.com>
2021-04-07 02:36:33 -0300

    Add files via upload

--- inc/ceo.php
+++ inc/ceo.php
@@ -1,1 +1,20 @@
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:site" content="@Gilbertsampaio">
+<meta name="twitter:creator" content="@Gilbertsampaio">
+<meta name="twitter:title" content="Sistema de Gerenciamento de Tarefas">
+<meta name="twitter:description" content="Administre as tarefas de sua empresa.">
+<meta name="twitter:image" content="<?php echo $url; ?>/img/tarefas.jpg">
 
+<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="author" content="Sistema de Gerenciamento de Tarefas" />
+<meta name="description" content="Administre as tarefas de sua empresa." />
+<meta property="og:url" content="<?php echo $url; ?>/tarefas.php" />
+<meta name="keywords" content="Tarefas, Tarefa, Afazeres, Chamados, Controle">
+<meta property="og:title" content="Sistema de Gerenciamento de Tarefas" />
+<meta property="og:site_name" content="Sistema de Gerenciamento de Tarefas">
+<meta property="og:description" content="Administre as tarefas de sua empresa." />
+<meta property="og:type" content="website">
+<meta property="og:image" itemprop="image" content="<?php echo $url; ?>/img/tarefas.jpg" />
+<link itemprop="thumbnailUrl" href="<?php echo $url; ?>/img/tarefas.jpg">
+<meta property="og:image:type" content="image/jpeg">
